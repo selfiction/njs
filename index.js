40fe2a4dfa40ea3c2a37d31f40d6eabf92501056
Arseniy <cojarsenij@gmail.com>
2024-06-01 04:54:29 +0500

    index.js source-code edited

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const http = require("http");
- 
-const server =  http.createServer(function(request, response){
-    response.end("Hello METANIT.COM!");
-});
-server.listen(3000, function(){ console.log("Сервер запущен по адресу http://localhost:3000")});
-Math.floor(Math.random() * 9999);
+const fs = require('fs')
+const http = require('http')
+
+const PORT = 3000
+const HOST = '127.0.0.1'
+
+let server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+
+    const stream = fs.createReadStream('./index.html')
+    stream.pipe(res)
+})
+
+let otp_code = Math.floor(Math.random() * 9999) + 1000;
+
+fs.writeFile('./code.txt', parseInt(otp_code).toString(), (err) => {
+    if (err){
+        console.log('OTP FAILURE')
+    }
+    console.log('OTP GENERATED')
+})
+console.log(otp_code)
+
+server.listen(PORT, HOST, () => {
+    console.log(`SERVER STARED AT http://${HOST}:${PORT}`)
+})
+
