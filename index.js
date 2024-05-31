b964d10749da7e8288b50c2b631b6873d22ec6ef
Arseniy <cojarsenij@gmail.com>
2024-06-01 04:07:40 +0500

    .html added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const http = require("http");
+ 
+const server =  http.createServer(function(request, response){
+    response.end("Hello METANIT.COM!");
+});
+server.listen(3000, function(){ console.log("Сервер запущен по адресу http://localhost:3000")});
 Math.floor(Math.random() * 9999);
